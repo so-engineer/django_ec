d1efbe79893abc815cce10c22e0ad8628183b405
sousou1216 <sousou1216@yahoo.co.jp>
2023-12-30 00:10:45 +0900

    heroku対応

--- ecapp/views.py
+++ ecapp/views.py
@@ -6,13 +6,13 @@ from .models import ItemModel
 class ItemList(ListView):
     template_name = 'list.html'
     model = ItemModel
+    # メソッドをオーバーライドし商品をid順に取得する
+    def get_queryset(self):
+        return ItemModel.objects.all().order_by('id')
 
 class ItemDetail(DetailView):
     template_name = 'detail.html'
     model = ItemModel
-    # メソッドをオーバーライドし商品をid順に取得する
-    def get_queryset(self):
-        return ItemModel.objects.all().order_by('id')
 
     # メソッドをオーバーライドしURLから取得されるオブジェクトに加えて最新のDBデータを取得する
     def get_context_data(self, **kwargs):
