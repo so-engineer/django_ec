dad74f1177e084c681383d15a7a828df10862b81
sousou1216 <sousou1216@yahoo.co.jp>
2024-01-01 00:48:33 +0900

    セッションにカートIDを保存することに伴う変更

--- ecapp/admin.py
+++ ecapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import ItemModel
+from .models import ItemModel, CartModel, CartItemModel
 
 admin.site.register(ItemModel)
+admin.site.register(CartModel)
+admin.site.register(CartItemModel)
--- ecapp/models.py
+++ ecapp/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 
 class ItemModel(models.Model):
-    title = models.CharField(null=True, blank=True) # 必須フィールド
     name = models.CharField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     item_image = models.ImageField(upload_to="", null=True, blank=True)
     content = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.name
+    
+class CartModel(models.Model):
+    def __str__(self):
+        return self.id
+    
+class CartItemModel(models.Model):
+    # 中間テーブルの作成
+    cart = models.ForeignKey(CartModel, on_delete=models.CASCADE, related_name='cart_items')
+    item = models.ForeignKey(ItemModel, on_delete=models.CASCADE, related_name='item_carts')
+
+    def __str__(self):
+        return f"{self.cart}_{self.item}"
+    
