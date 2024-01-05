c8f3e167152533901cef25cf7b0cc2880961d65e
sousou1216 <sousou1216@yahoo.co.jp>
2024-01-05 21:24:44 +0900

    コメント対応

--- ecapp/admin.py
+++ ecapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import ItemModel, CartModel, CartItemModel, BillModel, BuyDetailModel, PromoCodeModel
+from .models import ItemModel, CartModel, CartItemModel, BillModel, BuyItemModel, PromoCodeModel
 
 admin.site.register(ItemModel)
 admin.site.register(CartModel)
 admin.site.register(CartItemModel)
 admin.site.register(BillModel)
-admin.site.register(BuyDetailModel)
+admin.site.register(BuyItemModel)
 admin.site.register(PromoCodeModel)
