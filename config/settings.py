f39b6c57439b1a63b7d2e495a0a68856b5bdd3a4
sousou1216 <sousou1216@yahoo.co.jp>
2025-06-12 06:11:27 +0900

    Add linter and formatter

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import environ
-
 from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,26 +53,26 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
-    'ecapp.apps.EcappConfig',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
+    "ecapp.apps.EcappConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # 追加
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # 追加
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # 'basicauth.middleware.BasicAuthMiddleware', # トップページにベーシック認証がかかる
 ]
 
@@ -83,25 +83,25 @@ MIDDLEWARE = [
 # キャッシュベースのセッション
 # SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'config/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "config/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -118,16 +118,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -135,9 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # CSS等のURLとなり最後スラッシュを入れる
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # CSS等の保存先（本番環境用となり本番環境はSTATICFILES_DIRSからここに一つにまとめる）
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -165,22 +165,22 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if IS_HEROKU_APP:
     # heroku上の画像表示に必要
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-    CLOUDINARY_STORAGE  = {
-        'CLOUD_NAME':'hhf8qu12k',
-        'API_KEY': env('CLOUDINARY_API_KEY'),
-        'API_SECRET': env('CLOUDINARY_API_SECRET')
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": "hhf8qu12k",
+        "API_KEY": env("CLOUDINARY_API_KEY"),
+        "API_SECRET": env("CLOUDINARY_API_SECRET"),
     }
 
 # ベーシック認証用
 BASICAUTH_USERS = {"admin": "pw"}
 
 # メール設定
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
--- ecapp/urls.py
+++ ecapp/urls.py
@@ -1,24 +1,59 @@
-from django.urls import path
-from .views import ItemList, ItemDetail, BillCreate, \
-     cart_func, add_to_cart_from_list_func, add_to_cart_from_detail_func, remove_from_cart_func, \
-     AdmItemList, AdmItemCreate, AdmItemUpdate, AdmItemDelete, AdmBuyList, AdmBuyDetail
 # ベーシック認証用
 from basicauth.decorators import basic_auth_required
+from django.urls import path
+
+from .views import (
+    AdmBuyDetail,
+    AdmBuyList,
+    AdmItemCreate,
+    AdmItemDelete,
+    AdmItemList,
+    AdmItemUpdate,
+    BillCreate,
+    ItemDetail,
+    ItemList,
+    add_to_cart_from_detail_func,
+    add_to_cart_from_list_func,
+    cart_func,
+    remove_from_cart_func,
+)
 
 urlpatterns = [
-    path('', ItemList.as_view(), name='list'),
-    path('detail/<int:pk>', ItemDetail.as_view(), name='detail'),
-    path('checkout/cart/', cart_func, name='checkout_cart'),
-    path('checkout/add_list/<int:pk>', add_to_cart_from_list_func, name='checkout_add_list'),
-    path('checkout/add_detail/<int:pk>', add_to_cart_from_detail_func, name='checkout_add_detail'),
-    path('checkout/remove/<int:pk>', remove_from_cart_func, name='checkout_remove'),
-    path('checkout/bill/', BillCreate.as_view(), name="checkout_bill"),
-
-    path('adm/items/', basic_auth_required(AdmItemList.as_view()), name="adm_list"),
-    path('adm/items/create/', basic_auth_required(AdmItemCreate.as_view()), name="adm_create"),
-    path('adm/items/update/<int:pk>', basic_auth_required(AdmItemUpdate.as_view()), name="adm_update"),
-    path('adm/items/delete/<int:pk>', basic_auth_required(AdmItemDelete.as_view()), name="adm_delete"),
-    path('adm/bills/', basic_auth_required(AdmBuyList.as_view()), name="adm_buy_list"),
-    path('adm/bills/<int:pk>/items/', basic_auth_required(AdmBuyDetail.as_view()), name="adm_buy_detail"),
-
+    path("", ItemList.as_view(), name="list"),
+    path("detail/<int:pk>", ItemDetail.as_view(), name="detail"),
+    path("checkout/cart/", cart_func, name="checkout_cart"),
+    path(
+        "checkout/add_list/<int:pk>",
+        add_to_cart_from_list_func,
+        name="checkout_add_list",
+    ),
+    path(
+        "checkout/add_detail/<int:pk>",
+        add_to_cart_from_detail_func,
+        name="checkout_add_detail",
+    ),
+    path("checkout/remove/<int:pk>", remove_from_cart_func, name="checkout_remove"),
+    path("checkout/bill/", BillCreate.as_view(), name="checkout_bill"),
+    path("adm/items/", basic_auth_required(AdmItemList.as_view()), name="adm_list"),
+    path(
+        "adm/items/create/",
+        basic_auth_required(AdmItemCreate.as_view()),
+        name="adm_create",
+    ),
+    path(
+        "adm/items/update/<int:pk>",
+        basic_auth_required(AdmItemUpdate.as_view()),
+        name="adm_update",
+    ),
+    path(
+        "adm/items/delete/<int:pk>",
+        basic_auth_required(AdmItemDelete.as_view()),
+        name="adm_delete",
+    ),
+    path("adm/bills/", basic_auth_required(AdmBuyList.as_view()), name="adm_buy_list"),
+    path(
+        "adm/bills/<int:pk>/items/",
+        basic_auth_required(AdmBuyDetail.as_view()),
+        name="adm_buy_detail",
+    ),
 ]
